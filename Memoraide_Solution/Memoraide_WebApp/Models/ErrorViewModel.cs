9739df2f199ad88ca61ea7040388e4381126c920
e-gaudet <30503123+e-gaudet@users.noreply.github.com>
2018-10-06 15:34:11 -0400

    Add files via upload

---
+++ Memoraide_Solution/Memoraide_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Memoraide_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
