48206a73e7714c10d232ca92a22545600c706701
wertville <wertville@live.ca>
2018-10-13 22:41:03 -0400

    Card landing page + basic create card page

---
+++ Memoraide_Solution/Memoraide_WebApp/Controllers/CardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Memoraide_WebApp.Controllers
+{
+    public class CardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
