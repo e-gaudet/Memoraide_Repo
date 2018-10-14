1f6d58bcf28c69543db7a394b6a58388741276b2
wertville <wertville@live.ca>
2018-10-13 22:52:19 -0400

    Card model

---
+++ Memoraide_Solution/Memoraide_WebApp/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memoraide_WebApp.Models
+{
+    public class Card
+    {
+        private int ID;
+        private string cardFront;
+        private string cardBack;
+    }
+}
