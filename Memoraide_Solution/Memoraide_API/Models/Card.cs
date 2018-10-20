e1354962d488639a40280ce666a264d1f5bfc028
dfloca <dfloca@users.noreply.github.com>
2018-10-20 17:03:36 -0400

    Created API Project

---
+++ Memoraide_Solution/Memoraide_API/Models/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Memoraide_API.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        private int deckId { get; set; }
+        private string question { get; set; }
+        private string answer { get; set; }
+    }
+}
