552f9c712510a270702b44e256e808193d5490fb
wertville <wertville@live.ca>
2018-10-21 04:21:08 -0400

    Pushing changes from last week that didn't get pushed up for some reason

--- Memoraide_Solution/Memoraide_WebApp/Controllers/CardController.cs
+++ Memoraide_Solution/Memoraide_WebApp/Controllers/CardController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Memoraide_WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Memoraide_WebApp.Controllers
@@ -16,6 +17,16 @@ namespace Memoraide_WebApp.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("CardFront,CardBack")] CardViewModel model)
+        {
+            if (ModelState.IsValid)
+                return RedirectToAction("Create");
+            else 
+                return View(model);
         }
     }
 }
---
+++ Memoraide_Solution/Memoraide_WebApp/Models/CardViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Memoraide_WebApp.Models
+{
+    public class CardViewModel
+    {
+        public int ID;
+
+        [Required]
+        [Display(Name = "Card Front")]
+        [MaxLength(1024)]
+        public string CardFront { get; set; }
+
+        [Required]
+        [Display(Name = "Card Back")]
+        [MaxLength(1024)]
+        public string CardBack { get; set; }
+    }
+}
