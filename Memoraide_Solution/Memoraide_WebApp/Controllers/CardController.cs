b6dc08659ad4f5955fad30978bbd46f899682431
ethan gaudet <ethangaudet@hotmail.com>
2018-10-27 18:03:33 -0400

    Added details page

--- Memoraide_Solution/Memoraide_WebApp/Controllers/CardController.cs
+++ Memoraide_Solution/Memoraide_WebApp/Controllers/CardController.cs
@@ -80,32 +80,32 @@ namespace Memoraide_WebApp.Controllers
             }
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> ViewCard(int id)
-        //{
-        //    string url = "https://localhost:44356/Cards/" + id;
+        [HttpGet]
+        public async Task<IActionResult> ViewCardDetail(int? id)
+        {
+            string url = "https://localhost:44356/Cards/" + id;
 
-        //    var response = await client.GetAsync(url);
+            var response = await client.GetAsync(url);
 
-        //    if (response.IsSuccessStatusCode)
-        //    {
-        //        var jsonstring = response.Content.ReadAsStringAsync();
-        //        jsonstring.Wait();
-        //        CardViewModel model = JsonConvert.DeserializeObject<CardViewModel>(jsonstring.Result);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonstring = response.Content.ReadAsStringAsync();
+                jsonstring.Wait();
+                CardViewModel model = JsonConvert.DeserializeObject<CardViewModel>(jsonstring.Result);
 
-        //        if (model.Question == null)
-        //        {
-        //            model.Question = "test";
-        //            model.Answer = "test";
-        //        }
-        //        return View(model);
-        //    }
-        //    else
-        //    {
-        //        TempData["message"] = "Unable to grab card data";
-        //        return NotFound();
-        //    }
-        //}
+                if (model.Question == null)
+                {
+                    model.Question = "test";
+                    model.Answer = "test";
+                }
+                return View(model);
+            }
+            else
+            {
+                TempData["message"] = "Unable to grab card data";
+                return NotFound();
+            }
+        }
 
         [HttpPut]
         public async Task<IActionResult> EditCard([Bind("ID")] int id, [Bind("ID,Question,Answer,CardTags,DeckId")] CardViewModel model)
